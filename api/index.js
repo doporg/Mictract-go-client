04df94bb02badba01176c68f4355d3ed12db64eb
TangliziGit <tanglizimail@foxmail.com>
2021-03-02 20:26:24 +0800

    feat(api): new api for channel and peer

--- api/index.js
+++ api/index.js
@@ -37,6 +37,26 @@ const api = {
       Api('DELETE', `/api/user`)
           .body(url)
           .done(),
+
+
+  // channel
+  listChannel: () =>
+      Api('GET', `/api/channel`).done(),
+
+  createChannel: (channel) =>
+      Api('POST', `/api/channel`)
+          .body(channel)
+          .done(),
+
+  deleteChannel: (url) =>
+      Api('DELETE', `/api/channel`)
+          .body(url)
+          .done(),
+
+  listPeersByNetwork: (network) =>
+      Api('GET', `/api/peer`)
+          .query({ network })
+          .done(),
 }
 
 export default api;
